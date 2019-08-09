5604fdae5f7289b6e3a25f8be00aba0245f2138d
slarsar <slarsar@yandex.com>
2019-08-10 07:30:30 +0800

    format code structure

--- validSoduku/validSoduku.go
+++ validSoduku/validSoduku.go
@@ -16,42 +16,40 @@ func main() {
 
 	input2 := [][]byte{
 
-	{'8','3','.','.','7','.','.','.','.'},
-	{'6','.','.','1','9','5','.','.','.'},
-	{'.','9','8','.','.','.','.','6','.'},
-	{'8','.','.','.','6','.','.','.','3'},
-	{'4','.','.','8','.','3','.','.','1'},
-	{'7','.','.','.','2','.','.','.','6'},
-	{'.','6','.','.','.','.','2','8','.'},
-	{'.','.','.','4','1','9','.','.','5'},
-	{'.','.','.','.','8','.','.','7','9'},
-
+		{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
 	}
 
 	isS := isValidSudoku(input2)
 	println(isS)
 
 }
-func printBigByte(input [][]byte){
+func printBigByte(input [][]byte) {
 	for _, item := range input {
 		printByte(item)
 	}
 }
 
-func printByte(input []byte){
+func printByte(input []byte) {
 	for _, val := range input {
-		print(string(val)," ")
+		print(string(val), " ")
 	}
 	println()
 }
 
 func validLine(line []byte) bool {
 	for index, _ := range line {
-		for _ , val := range line[index + 1:] {
-			if val == byte('.'){
+		for _, val := range line[index+1:] {
+			if val == byte('.') {
 				continue
 			}
-
 
 			if line[index] == val {
 				return false
@@ -105,7 +103,6 @@ func getGrids(board [][]byte) (ret [][]byte) {
 		ret[index] = make([]byte, len(board[0]))
 	}
 
-
 	for i := 0; i < 9; i++ {
 		var retIndex int
 		for index1, value1 := range board {
@@ -124,22 +121,18 @@ func getGrids(board [][]byte) (ret [][]byte) {
 	return
 }
 
-
 func getRows(board [][]byte) (ret [][]byte) {
-
 
 	ret = make([][]byte, len(board))
 	for index, _ := range ret {
 		ret[index] = make([]byte, len(board[0]))
 	}
 
-
-	for index1, value1 := range board{
+	for index1, value1 := range board {
 		for index2, value2 := range value1 {
 			ret[index2][index1] = value2
 		}
 	}
-
 
 	return
 
