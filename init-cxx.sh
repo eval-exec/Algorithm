0c1b665c3327b6f26f66218bc7aec609ff677085
slarsar <slarsar@yandex.com>
2020-10-05 09:40:14 +0800

    add init cpp project script

---
+++ init-cxx.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+set -evx
+if [ -z "$1" ]; then
+  echo "need one argument"
+  exit 1
+fi
+
+dirname=$1
+mkdir $dirname
+touch $dirname/main.cxx
+
+echo "cmake_minimum_required(VERSION 3.17)
+project(leetcode_solutions)
+
+set(CMAKE_CXX_STANDARD 17)
+
+add_executable($dirname main.cxx)
+" >$dirname/CMakeLists.txt
+
+echo "add_subdirectory($dirname)" >>CMakeLists.txt
