685e7fd35800dc8985a49b1e69fcfee70fe40a1d
slarsar <slarsar@yandex.com>
2020-10-22 15:15:45 +0800

    understand good solution from discuss, populate next tight pointers in each nodes II

--- Populate_Next_Right_Pointers_In_Each_Node_II/main.cxx
+++ Populate_Next_Right_Pointers_In_Each_Node_II/main.cxx
@@ -201,3 +201,26 @@ int main() {
 }
 
 
+void connect(Node *root) {
+    Node *now, *tail, *head;
+
+    now = root;
+    head = tail = NULL;
+    while (now) {
+        if (now->left) {
+            if (tail) {
+                tail = tail->next = now->left;
+            } else {
+                head = tail = now->left;
+            }
+        }
+        if (now->right) {
+            if (tail) tail = tail->next = now->right;
+            else head = tail = now->right;
+        }
+        if (!(now = now->next)) {
+            now = head;
+            head = tail = NULL;
+        }
+    }
+}
