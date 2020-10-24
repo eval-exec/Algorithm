714803c72561736d5c45eaeece4399e718e1ce97
slarsar <slarsar@yandex.com>
2020-10-24 21:27:35 +0800

    solved memory recursion

---
+++ practice/main.cxx
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+class Solution {
+public:
+    int fib(int N) {
+        auto r = mm.find(N);
+        if (r != mm.end()){
+            return r->second;
+        }
+        if (N <= 1){
+            mm[N] = N;
+            return N;
+        }
+        int sum = fib(N-1) + fib(N-2);
+        mm[N] = sum;
+        return sum;
+        
+    }
+    private:
+map<int,int> mm;
+};
+
+int main() {
+    testing::InitGoogleTest();
+
+    return RUN_ALL_TESTS();
+}
+
+
