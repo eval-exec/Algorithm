cf6d787762978788e56cbf894502e34d92c53d95
slarsar <slarsar@yandex.com>
2020-11-01 19:12:39 +0800

    solved Find Minumum

---
+++ FindMinimum/main.cxx
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+
+        }
+        return left;
+
+    }
+};
+
+
+struct T {
+
+};
+
+TEST(Solution, test) {
+    T ts[] = {
+            {
+
+            },
+
+    };
+
+    Solution solution;
+
+    for (T t : ts) {
+
+    }
+}
+
+int main() {
+    testing::InitGoogleTest();
+
+    return RUN_ALL_TESTS();
+}
+
+
