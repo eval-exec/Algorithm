34bd805993ac18de3489dc475e141ca11fde8390
slarsar <slarsar@yandex.com>
2020-11-04 19:24:27 +0800

    solved   Delete Node in a Linked List

---
+++ DeleteNodeinaLinkedList/main.cxx
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode *node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+
+struct T {
+
+};
+
+TEST(Solution, test) {
+    T ts[] = {
+            {
+
+            },
+
+    };
+
+    Solution solution;
+
+    for (T t : ts) {
+
+    }
+}
+
+int main() {
+    testing::InitGoogleTest();
+
+    return RUN_ALL_TESTS();
+}
+
+
