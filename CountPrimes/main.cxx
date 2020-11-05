0d10e98feed793d986872b1d7af1b447a71fa9f5
slarsar <slarsar@yandex.com>
2020-11-05 16:52:25 +0800

    solved count primes

--- CountPrimes/main.cxx
+++ CountPrimes/main.cxx
@@ -6,52 +6,70 @@ using namespace std;
 
 class Solution {
 public:
+
     int countPrimes(int n) {
+        vector<bool> ss(n + 1);
+        for (int i = 2; i <= sqrt(n); i++) {
+            if (ss[i]) {
+                continue;
+            }
+            int x = 2;
+            while (x * i <= n) {
+                ss[i * x] = true;
+                x++;
+            }
+        }
         int count = 0;
-        n--;
-        while (n >= 2) {
-            if (isPrimer(n)) {
+        for (bool b : ss) {
+            if (!b) {
+
                 count++;
             }
-            n--;
+
         }
-        return count;
+        return count - 2;
     }
 
-private:
-    bool isPrimer(int n) {
-        if (n == 2) {
-            return true;
-        }
-
-        for (int i = 2; i <= sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
 };
 
 
 struct T {
     int n;
+    int expect;
 
 };
 
 TEST(Solution, test) {
     T ts[] = {
             {
-
-                    10
+                    1000000,
+                    78498
             },
+            {
+                    5000000,
+                    0,
+            },
+
+            {
+                    1500000,
+                    114155
+            },
+            {
+                    100,
+                    25,
+            },
+
+            {
+                    1000,
+                    168,
+            }
 
     };
 
     Solution solution;
 
     for (T t : ts) {
-        cout << solution.countPrimes(t.n) << endl;
+        EXPECT_EQ(solution.countPrimes(t.n), t.expect);
 
     }
 }
