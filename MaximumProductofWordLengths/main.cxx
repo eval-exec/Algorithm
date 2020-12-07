a5bd76b398fc5cc9952c5add3212e489f45d3eda
eval-exec <slarsar@yandex.com>
2020-12-07 17:54:02 +0800

    optimized

--- MaximumProductofWordLengths/main.cxx
+++ MaximumProductofWordLengths/main.cxx
@@ -6,6 +6,36 @@ using namespace std;
 class Solution {
   public:
     int maxProduct(vector<string> &words) {
+        sort(words.begin(), words.end(), [](const string &s1, const string s2) -> bool {
+            return s1.size() > s2.size();
+        });
+        vector<int> bits;
+        for (int i = 0; i < words.size(); i++) {
+            bitset<26 * 1000> sum;
+            for (int j = 0; j < words[i].size(); j++) {
+                bits[i] |= (1 << (words[i][j] - 'a' + 1));
+            }
+        }
+
+        int max_p = 0;
+        for (int i = 0; i < words.size() - 1; i++) {
+            for (int j = i + 1; j < words.size(); j++) {
+                if ((bits[i] & bits[j]) == 0) {
+                    max_p = max(max_p, int(words[i].size() * words[j].size()));
+                    if (max_p >= words[i].size() * words[i].size()) return max_p;
+                }
+            }
+        }
+        return max_p;
+    }
+};
+
+class Solution1 {
+  public:
+    int maxProduct(vector<string> &words) {
+        sort(words.begin(), words.end(), [](const string &s1, const string s2) -> bool {
+            return s1.size() < s2.size();
+        });
         vector<set<int>> memo(26);
         for (int i = 0; i < words.size(); i++) {
             for (const char &c : words[i]) {
@@ -21,8 +51,11 @@ class Solution {
                 auto s2 = memo[c - 'a'];
                 ss.insert(s2.begin(), s2.end());
             }
-            for (int j = 0; i < words.size() && j != i && ss.count(j) == 0; j++) {
+
+            for (int j = words.size() - 1; j > i; j--) {
+                if (ss.count(j) != 0) continue;
                 max_p = max(max_p, int(words[i].size() * words[j].size()));
+                break;
             }
         }
         return max_p;
@@ -50,6 +83,22 @@ TEST(Solution, test) {
 
 int main() {
     testing::InitGoogleTest();
+    ofstream f;
+    f.open("tmp.txt");
+
+    for (int i = 0; i < 1000; i++) {
+        int word_len = rand() % 1000;
+//        int word_len = 4;
+        string word;
+        word.resize(word_len);
+        for (int j = 0; j < word_len; j++) {
+            int w = rand() % 26;
+            word[j] = char('a' + w);
+        }
+        f << '"' << word << '"' << ",";
+    }
+    f.flush();
+    f.close();
 
     return RUN_ALL_TESTS();
 }
