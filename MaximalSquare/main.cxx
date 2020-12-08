81cb0e9856b2c0f04112dca3427bbd1835f231da
eval-exec <slarsar@yandex.com>
2020-12-08 15:07:22 +0800

    optimized maximal square

--- MaximalSquare/main.cxx
+++ MaximalSquare/main.cxx
@@ -3,9 +3,6 @@
 
 using namespace std;
 
-
-
-
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
@@ -30,6 +27,38 @@ public:
                     max_area = max(max_area,dp[i][j]*dp[i][j]);
                 }
             }
+        }
+        return max_area;
+    }
+};
+
+
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty()) return 0;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<int> dp(cols,0);
+        int max_area=0;
+        for (int i = 0;i < cols;i++){
+            dp[i] = matrix[0][i] == '1'?1:0;
+            if (dp[i]==1) max_area = 1;
+        }
+        for (int i = 1; i< rows;i++){
+            vector<int> tmp(cols,0);
+            for (int j = 0; j< cols;j++){
+                if (matrix[i][j] == '1'){
+                    int len = 1;
+                    if (j > 0){
+                        len = 1+min(tmp[j-1],dp[j]);
+                    }
+                    tmp[j] = matrix[i-len+1][j-len+1] == '1'? len:len-1;
+                    max_area = max(max_area,tmp[j]*tmp[j]);
+                }
+            }
+            dp = tmp;
         }
         return max_area;
     }
