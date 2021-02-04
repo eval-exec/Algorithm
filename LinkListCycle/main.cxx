58870d6e89a6aaca3899e0a8c61bf865e5e1574f
eval-exec <slarsar@yandex.com>
2021-02-04 16:57:14 +0800

    solved link list cycle detect

---
+++ LinkListCycle/main.cxx
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast && fast->next && fast->next->next){
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+};
+
+
+
+struct T{
+
+};
+
+TEST(Solution,test){
+    T ts[] = {
+        {
+
+        },
+
+    };
+
+
+    for (T t : ts){
+        Solution solution;
+
+    }
+}
+
+int main() {
+    testing::InitGoogleTest();
+
+    return RUN_ALL_TESTS();
+}
+
+
