7abdc55769bdca609187c6fc5d11393de8f29b39
eval-exec <slarsar@yandex.com>
2021-02-14 00:23:28 +0800

    solved check if array is sorted and rotated, optimaze beats 100%

--- LeetCode/MaximumScoreFromRemovingStones/main.cxx
+++ LeetCode/MaximumScoreFromRemovingStones/main.cxx
@@ -3,40 +3,55 @@
 
 using namespace std;
 
-
 class Solution {
-public:
+  public:
     int maximumScore(int a, int b, int c) {
-        vector<int> nums = {a,b,c};
-        sort(nums.begin(),nums.end());
-        int scores = 0;
-        while(nums[2] != 0 && nums[1] != 0){
+        vector<int> nums = {a, b, c};
+        sort(nums.begin(), nums.end());
+        while (nums[2] != 0 && nums[1] != 0) {
             proc(nums);
-            scores++;
-            sort(nums.begin(),nums.end());
+            sort(nums.begin(), nums.end());
         }
         return scores;
     }
-private:
-    void proc(vector<int> &nums){
-        if (nums[0] != 0 ){
-            nums[0]--;
-        }else if (nums[1] != 0){
-            nums[1]--;
-        }else{
+  private:
+    int scores = 0;
+    void proc(vector<int> &nums) {
+        if (nums[0] != 0) {
+            if (nums[1] == nums[2]) {
+                int n1 = nums[0] / 2;
+                int n2 = nums[0] - n1;
+                scores += nums[0];
+                nums[0] = 0;
+                nums[1] -= n2;
+                nums[2] -= n1;
+            } else {
+                int delta = nums[2] - nums[1];
+                if (delta >= nums[0]) {
+                    scores +=  nums[0];
+                    nums[2] -= nums[0];
+                    nums[0] = 0;
+                } else {
+                    scores += delta;
+                    nums[0] -= delta;
+                    nums[2] -= delta;
+                }
+            }
+        } else if (nums[1] != 0) {
+            scores += min(nums[1], nums[2]);
+            nums[2] -= nums[1];
+            nums[1] = 0;
+        } else {
             return;
         }
-        nums[2]--;
     }
 };
 
-
-
-struct T{
+struct T {
 
 };
 
-TEST(Solution,test){
+TEST(Solution, test) {
     T ts[] = {
         {
 
@@ -44,8 +59,7 @@ TEST(Solution,test){
 
     };
 
-
-    for (T t : ts){
+    for (T t : ts) {
         Solution solution;
 
     }
