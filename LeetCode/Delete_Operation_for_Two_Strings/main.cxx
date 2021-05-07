cad0d4462dd098195f2698d91c10486d72c317bb
eval-exec <execvy@gmail.com>
2021-05-07 17:52:04 +0800

    optimize daily dp from 2d array to 1d array
    See:
    
    Why:
    
    How:
    
    Tags:

--- LeetCode/Delete_Operation_for_Two_Strings/main.cxx
+++ LeetCode/Delete_Operation_for_Two_Strings/main.cxx
@@ -11,13 +11,36 @@ using namespace std;
 class Solution {
   public:
     int minDistance(string word1, string word2) {
+        vector<int> dp(vector<int>(word2.size()));
+        dp[0] = word1[0] == word2[0] ? 0 : 2;
+        for (int i = 1; i < word2.size(); i++) {
+            dp[i] = word1[0] == word2[i] ? i : dp[i - 1] + 1;
+        }
+        for (int i = 1; i < word1.size(); i++) {
+            vector<int> tmp(dp);
+            dp[0] = word1[i] == word2[0] ? i : tmp[0] + 1;
+            for (int j = 1; j < word2.size(); j++) {
+                if (word1[i] == word2[j]) {
+                    dp[j] = tmp[j - 1];
+                } else {
+                    dp[j] = 1 + min(tmp[j], dp[j - 1]);
+                }
+            }
+        }
+        return dp.back();
+    }
+};
+
+class Solution_1 {
+  public:
+    int minDistance(string word1, string word2) {
         vector<vector<int>> dp(word1.size(), vector<int>(word2.size()));
         dp[0][0] = word1[0] == word2[0] ? 0 : 2;
         for (int i = 1; i < word2.size(); i++) {
             dp[0][i] = word1[0] == word2[i] ? i : dp[0][i - 1] + 1;
         }
         for (int i = 1; i < word1.size(); i++) {
-            dp[i][0] = word2[0] == word1[i] ? i : dp[i-1][0] + 1;
+            dp[i][0] = word2[0] == word1[i] ? i : dp[i - 1][0] + 1;
         }
         for (int i = 1; i < word1.size(); i++) {
             for (int j = 1; j < word2.size(); j++) {
@@ -31,7 +54,6 @@ class Solution {
         return dp.back().back();
     }
 };
-
 //// END
 struct T {
 
