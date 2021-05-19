4a2142cd8b749d5f8eb915e6e8639ef30de06d47
eval-exec <execvy@gmail.com>
2021-05-19 16:25:24 +0800

    solved leetcode daily challenge, minimum move to equal array elements ii

---
+++ LeetCode/Minimum_Moves_to_Equal_Array_Elements_II/main.cxx
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#include <gtest/gtest.h>
+using namespace std;
+
+
+//// START
+/*
+## Minimum Moves to Equal Array Elements II
+
+*/
+
+
+class Solution {
+  public:
+    int minMoves2(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        long low = nums.front();
+        long high = nums.back();
+        vector<long> base_low(nums.size());
+        vector<long> base_high(nums.size());
+
+        long sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i] - low;
+            base_low[i] = sum;
+        }
+        sum = 0;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            sum += high - nums[i];
+            base_high[i] = sum;
+        }
+        long ret = INT_MAX;
+        for (int i = 0; i < nums.size(); i++) {
+            long left = (nums[i] - low) * (i + 1) - base_low[i];
+            long right = (high - nums[i]) * (nums.size() - i - 1);
+            if (i + 1 < nums.size()) right -= base_high[i + 1];
+            ret = min(ret, left + right);
+        }
+        return ret;
+    }
+};
+
+//// END
+struct T {
+
+};
+
+TEST(Solution, test) {
+    T ts[] = {
+        {
+
+        },
+        {
+
+        },
+
+    };
+
+    for (T t : ts) {
+        Solution solution;
+
+    }
+}
+
+int main() {
+    testing::InitGoogleTest();
+
+    return RUN_ALL_TESTS();
+}
+
+
